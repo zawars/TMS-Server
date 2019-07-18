966fe4984d9f68d7619222c8e0dfcb187ab3f1e5
Saad Afzaal <saad.afzaal777@gmail.com>
2019-07-18 15:48:31 +0500

    create ratesheets apis

--- api/controllers/RateSheetsController.js
+++ api/controllers/RateSheetsController.js
@@ -6,7 +6,18 @@
  */
 
 module.exports = {
-  
+
+  getBatchRateSheets: async (req, res) => {
+    try {
+      let rateSheetsIds = req.rateSheetsIds;
+
+      const rateSheets = await RateSheets.find({ id: { in: rateSheetsIds } }).populate('rates');
+
+      res.ok(rateSheets);
+    } catch (e) {
+      res.badRequest(e);
+    }
+  }
 
 };
 
