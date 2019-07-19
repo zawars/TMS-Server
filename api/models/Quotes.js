7a09499d5fed4ef903254d145a7d2baa844e2491
Saad Afzaal <saad.afzaal777@gmail.com>
2019-07-19 21:17:19 +0500

    create quote apis

---
+++ api/models/Quotes.js
@@ -0,0 +1,123 @@
+/**
+ * Quotes.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+    quoteNumber: {
+      type: 'number',
+      unique: true
+    },
+    name: {
+      type: 'string'
+    },
+    quoteDate: {
+      type: 'string'
+    },
+    pickUpDate: {
+      type: 'string'
+    },
+    deliveryDate: {
+      type: 'string'
+    },
+    description: {
+      type: 'string'
+    },
+    huCNT: {
+      type: 'string'
+    },
+    pieces: {
+      type: 'string'
+    },
+    nmFC: {
+      type: 'string'
+    },
+    weight: {
+      type: 'string'
+    },
+    length: {
+      type: 'string'
+    },
+    width: {
+      type: 'string'
+    },
+    height: {
+      type: 'string'
+    },
+    cube: {
+      type: 'string'
+    },
+    density: {
+      type: 'string'
+    },
+    hazmatUN: {
+      type: 'string'
+    },
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+    customer: {
+      model: 'tradingPartners'
+    },
+    pickUpFromCity: {
+      model: 'dropdownMapper'
+    },
+    pickUpFromCountry: {
+      model: 'dropdownMapper'
+    },
+    deliverToCity: {
+      model: 'dropdownMapper'
+    },
+    deliverToCountry: {
+      model: 'dropdownMapper'
+    },
+    product: {
+      model: 'dropdownMapper'
+    },
+    handlingUnit: {
+      model: 'dropdownMapper'
+    },
+    lengthUnit: {
+      model: 'dropdownMapper'
+    },
+    cubeUnit: {
+      model: 'dropdownMapper'
+    },
+    cubeUnit: {
+      model: 'dropdownMapper'
+    },
+    cuFt: {
+      model: 'dropdownMapper'
+    },
+    class: {
+      model: 'dropdownMapper'
+    },
+    accessorials: {
+      model: 'dropdownMapper'
+    },
+    rate: {
+      model: 'rates'
+    },
+    vendor: {
+      model: 'tradingPartners'
+    },
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
