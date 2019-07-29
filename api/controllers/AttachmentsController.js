ed16da267bad943cb2a4477f994340512e7c321f
Zawar Shahid <zawar_shahid93@hotmail.com>
2019-07-29 08:01:28 -0700

    Bug fixed

--- api/controllers/AttachmentsController.js
+++ api/controllers/AttachmentsController.js
@@ -8,7 +8,7 @@
 module.exports = {
   create: (req, res) => {
     req.file('attachment').upload({
-      dirname: '../../../uploads/'
+      dirname: '../../../api/v1/uploads/'
     }, async (err, uploadedFiles) => {
       if (err) {
         return res.send(500, err);
