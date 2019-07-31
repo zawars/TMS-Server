d1efc76e7ff2df87d4e56272e88766a4c231c182
Zawar Shahid <zawar_shahid93@hotmail.com>
2019-07-31 07:12:12 -0700

    POST API added for shipment tracking

--- config/routes.js
+++ config/routes.js
@@ -100,4 +100,7 @@ module.exports.routes = {
   // Markup
   'POST /api/v1/markup': 'MarkupController.create',
   'PATCH /api/v1/markup/:id': 'MarkupController.update',
+
+  // Shipment Tracking
+  'POST /api/v1/shipmentTracking': 'ShipmentTrackingController.create',
 };
