7f90dd5c12ef6d67bd15e634079c3105578e4451
Zawar Shahid <zawar_shahid93@hotmail.com>
2019-08-22 09:30:40 -0700

    Api fixed for performance issues

--- api/controllers/DropdownMapperChildController.js
+++ api/controllers/DropdownMapperChildController.js
@@ -10,18 +10,28 @@ module.exports = {
   searchCity: async (req, res) => {
     try {
       let query = req.params.query;
-
+      let result;
       let dropDown = await Dropdown.findOne({
         field: 'Cities'
       });
+      let val = +query;
 
-      const cities = await DropdownMapper.find({
-        dropdown: dropDown.id,
-        name: {
-          'contains': query
-        }
-      }).populateAll().limit(50);
-      res.ok(cities);
+      if (Number.isNaN(val)) {
+        result = await DropdownMapper.find({
+          dropdown: dropDown.id,
+          name: {
+            'contains': query
+          }
+        }).populateAll().limit(10);
+      } else {
+        result = await DropdownMapperChild.find({
+          name: {
+            'contains': query
+          }
+        }).populateAll();
+      }
+
+      res.ok(result);
     } catch (e) {
       res.ok(e);
     }
