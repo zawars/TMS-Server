51ac376bf826001c4123c0a39c8febbecc9b64ab
Saad Afzaal <saadafzal@akkastech.com>
2019-10-17 15:45:58 +0500

    change in contract api

--- api/controllers/ContractsController.js
+++ api/controllers/ContractsController.js
@@ -8,34 +8,34 @@
 module.exports = {
   create: async (req, res) => {
     let contract = req.body;
-    let rateSheetList = contract.rateSheets;
-    let accessorialList = contract.accessorials;
-    delete(contract.rateSheets);
-    delete(contract.accessorials);
+    // let rateSheetList = contract.rateSheets;
+    // let accessorialList = contract.accessorials;
+    // delete(contract.rateSheets);
+    // delete(contract.accessorials);
 
     let contractObj = await Contracts.create(contract).fetch();
 
-    let ratesCollection = [];
-    rateSheetList.map(rateSheet => {
-      rateSheet.contract = contractObj.id
-      ratesCollection.push(rateSheet.rates);
-      delete(rateSheet.rates);
-    });
+    // let ratesCollection = [];
+    // rateSheetList.map(rateSheet => {
+    //   rateSheet.contract = contractObj.id
+    //   ratesCollection.push(rateSheet.rates);
+    //   delete(rateSheet.rates);
+    // });
 
-    accessorialList.map(accessorial => {
-      accessorial.contract = contractObj.id
-    });
+    // accessorialList.map(accessorial => {
+    //   accessorial.contract = contractObj.id
+    // });
 
-    let rateSheets = await RateSheets.createEach(rateSheetList).fetch();
-    let accessorials = await Accessorials.createEach(accessorialList).fetch();
+    // let rateSheets = await RateSheets.createEach(rateSheetList).fetch();
+    // let accessorials = await Accessorials.createEach(accessorialList).fetch();
 
-    ratesCollection.map(async (rateList, idx) => {
-      rateList.map(rate => rate.rateSheet = rateSheets[idx].id);
-      rateSheets[idx].rates = await Rates.createEach(rateList).fetch();
-    });
+    // ratesCollection.map(async (rateList, idx) => {
+    //   rateList.map(rate => rate.rateSheet = rateSheets[idx].id);
+    //   rateSheets[idx].rates = await Rates.createEach(rateList).fetch();
+    // });
 
-    contractObj.rateSheets = rateSheets;
-    contractObj.accessorials = accessorials;
+    // contractObj.rateSheets = rateSheets;
+    // contractObj.accessorials = accessorials;
 
     res.ok(contractObj);
   },
