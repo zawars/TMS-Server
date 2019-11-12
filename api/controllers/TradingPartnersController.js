5352b7869afc2f562f9f7bcfbac0e604e30478bd
Zawar Shahid <zawar_shahid93@hotmail.com>
2019-11-12 19:56:25 +0500

    update api tweaked for trading partner

--- api/controllers/TradingPartnersController.js
+++ api/controllers/TradingPartnersController.js
@@ -124,15 +124,44 @@ module.exports = {
 
   update: async (req, res) => {
     let data = req.body;
-    let locations = data.locations;
+    let customerLocations = data.customerLocations;
     let products = data.products;
-    delete(data.locations);
+    let vendorLocations = data.vendorLocations;
+    let thirdPartyLocations = data.thirdPartyLocations;
+    let thirdPartyBillToList = data.thirdPartyBillTo;
+    let usersList = data.users;
+    delete(data.customerLocations);
     delete(data.products);
+    delete(data.vendorLocations);
+    delete(data.thirdPartyLocations);
+    delete(data.thirdPartyBillTo);
+    delete(data.users);
 
+    data.thirdPartyBillTo = [];
+    thirdPartyBillToList.forEach(val => {
+      data.thirdPartyBillTo.push(val.id);
+    });
+
+    // Trading Partner
     data.postalCode = data.postalCode.id;
     data.city = data.city.id;
-    data.billingCity = data.billingCity.id;
-    data.billingZipCode = data.billingZipCode.id;
+    data.state = data.state.id != undefined ? data.state.id : data.state;
+    data.country = data.country.id != undefined ? data.country.id : data.country;
+    // Customer
+    data.customerBillingPostalCode = data.customerBillingPostalCode.id != undefined ? data.customerBillingPostalCode.id : data.customerBillingPostalCode;
+    data.customerBillingCity = data.customerBillingCity.id != undefined ? data.customerBillingCity.id : data.customerBillingCity;
+    data.customerBillingState = data.customerBillingState.id != undefined ? data.customerBillingState.id : data.customerBillingState;
+    data.customerBillingCountry = data.customerBillingCountry.id != undefined ? data.customerBillingCountry.id : data.customerBillingCountry;
+    // Vendor
+    data.vendorBillingPostalCode = data.vendorBillingPostalCode.id != undefined ? data.vendorBillingPostalCode.id : data.vendorBillingPostalCode;
+    data.vendorBillingCity = data.vendorBillingCity.id != undefined ? data.vendorBillingCity.id : data.vendorBillingCity;
+    data.vendorBillingState = data.vendorBillingState.id != undefined ? data.vendorBillingState.id : data.vendorBillingState;
+    data.vendorBillingCountry = data.vendorBillingCountry.id != undefined ? data.vendorBillingCountry.id : data.vendorBillingCountry;
+    // Third Party
+    data.thirdPartyBillingPostalCode = data.thirdPartyBillingPostalCode.id != undefined ? data.thirdPartyBillingPostalCode.id : data.thirdPartyBillingPostalCode;
+    data.thirdPartyBillingCity = data.thirdPartyBillingCity.id != undefined ? data.thirdPartyBillingCity.id : data.thirdPartyBillingCity;
+    data.thirdPartyBillingState = data.thirdPartyBillingState.id != undefined ? data.thirdPartyBillingState.id : data.thirdPartyBillingState;
+    data.thirdPartyBillingCountry = data.thirdPartyBillingCountry.id != undefined ? data.thirdPartyBillingCountry.id : data.thirdPartyBillingCountry;
 
     let partner = await TradingPartners.update({
       id: req.params.id
@@ -140,7 +169,29 @@ module.exports = {
     partner = partner[0];
 
     let toBeCreatedLocations = [];
-    locations.forEach(location => {
+    customerLocations.forEach(location => {
+      if (location.id == undefined) {
+        location.tradingPartner = partner.id;
+        location.type = location.type.id;
+        location.state = location.state.id;
+        location.country = location.country.id;
+        location.city = location.city.id;
+        location.postalCode = location.postalCode.id;
+        toBeCreatedLocations.push(location);
+      }
+    });
+    vendorLocations.forEach(location => {
+      if (location.id == undefined) {
+        location.tradingPartner = partner.id;
+        location.type = location.type.id;
+        location.state = location.state.id;
+        location.country = location.country.id;
+        location.city = location.city.id;
+        location.postalCode = location.postalCode.id;
+        toBeCreatedLocations.push(location);
+      }
+    });
+    thirdPartyLocations.forEach(location => {
       if (location.id == undefined) {
         location.tradingPartner = partner.id;
         location.type = location.type.id;
@@ -153,8 +204,7 @@ module.exports = {
     });
 
     if (toBeCreatedLocations.length > 0) {
-      let locationsList = await Locations.createEach(toBeCreatedLocations).fetch();
-      partner.locations = locationsList;
+      await Locations.createEach(toBeCreatedLocations).fetch();
     }
 
     // Products section
@@ -170,8 +220,21 @@ module.exports = {
       });
 
       if (toBeCreatedProducts.length > 0) {
-        let productsList = await Products.createEach(toBeCreatedProducts).fetch();
-        partner.products = productsList;
+        await Products.createEach(toBeCreatedProducts).fetch();
+      }
+    }
+
+    // Users section
+    if (usersList) {
+      let toBeCreatedUsers = [];
+      usersList.forEach(user => {
+        if (user.id == undefined) {
+          toBeCreatedUsers.push(user);
+        }
+      });
+
+      if (toBeCreatedUsers.length > 0) {
+        await User.createEach(toBeCreatedUsers).fetch();
       }
     }
 
