a6f09e1c73dd11bc5bbc61e4aa5b3b23dbc09da5
Zawar Shahid <zawar_shahid93@hotmail.com>
2019-11-15 03:10:31 +0500

    Testing

--- api/controllers/ClaimsController.js
+++ api/controllers/ClaimsController.js
@@ -32,4 +32,48 @@ module.exports = {
     }
   },
 
+  create: async (req, res) => {
+    try {
+      let data = req.body;
+      let claim = await Claims.create(data).fetch();
+      let emailsList = [];
+
+      emailsList.push(data.contactEmail);
+
+      let organisation = await Organisation.findOne({
+        id: claim.customer.organisation
+      }).populateAll;
+
+      console.log(organisation)
+
+      EmailService.sendMail({
+        email: emailsList,
+        subject: `New Claim # ${responseClaim.uid}`,
+        message: `<p>
+        Dear Sir/Madam, <br><br>
+        Your claim has been registered. Please see details below. <br><br>
+
+        BOL: ${responseClaim.bolNumber} <br> PRO: ${responseClaim.proNumber} <br> Claimant Information <br> 
+        ${responseClaim.claimantName} <br> ${responseClaim.address1} <br> ${responseClaim.city.name} <br> 
+        ${responseClaim.postalCode.name} <br> ${responseClaim.phone} <br> ${responseClaim.email} <br>  <br> 
+
+        We will update you as soon as possible.<br>
+        Thank you for your understanding.<br><br>
+        </p>`
+      }, (err) => {
+        if (err) {
+          res.badRequest({
+            message: "Error sending email."
+          });
+        } else {
+          res.ok(claim);
+        }
+      });
+    } catch (error) {
+      res.badRequest({
+        error
+      });
+    }
+  },
+
 };
