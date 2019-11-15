fb79699028b323b8e33ddbcee59a33c90b5bd554
Zawar Shahid <zawar_shahid93@hotmail.com>
2019-11-16 03:55:26 +0500

    Bugs fixed

--- api/controllers/ClaimsController.js
+++ api/controllers/ClaimsController.js
@@ -33,60 +33,59 @@ module.exports = {
   },
 
   create: async (req, res) => {
-    // try {
-    let data = req.body;
-    let userEmail = data.userEmail;
-    delete(data.userEmail);
-    let claim = await Claims.create(data).fetch();
-    claim = await Claims.findOne({
-      id: claim.id
-    }).populateAll();
-    let emailsList = [];
+    try {
+      let data = req.body;
+      let userEmail = data.userEmail;
+      delete(data.userEmail);
+      let claim = await Claims.create(data).fetch();
+      claim = await Claims.findOne({
+        id: claim.id
+      }).populateAll();
+      let emailsList = [];
 
-    emailsList.push(data.contactEmail);
-    console.log('claim ', claim)
+      emailsList.push(data.contactEmail, userEmail);
 
-    let organisation = await Organisation.findOne({
-      id: claim.customer.organisation
-    }).populateAll();
-    console.log('org ', organisation)
+      let organisation = await Organisation.findOne({
+        id: claim.customer.organisation
+      }).populateAll();
 
-    emailsList.push(data.contactEmail);
-    organisation.users.forEach(user => {
-      if (user.roles.includes('Claim Manager')) {
-        emailsList.push(user.email);
+      emailsList.push(data.contactEmail);
+      if (organisation.users.length > 0) {
+        organisation.users.forEach(user => {
+          if (user.roles.includes('Claim Manager')) {
+            emailsList.push(user.email);
+          }
+        });
       }
-    });
-    console.log(emailsList)
 
-    EmailService.sendMail({
-      email: emailsList,
-      subject: `New Claim # ${responseClaim.uid}`,
-      message: `<p>
+      EmailService.sendMail({
+        email: emailsList,
+        subject: `New Claim # ${claim.uid}`,
+        message: `<p>
         Dear Sir/Madam, <br><br>
         Your claim has been registered. Please see details below. <br><br>
 
-        BOL: ${responseClaim.bolNumber} <br> PRO: ${responseClaim.proNumber} <br> Claimant Information <br> 
-        ${responseClaim.claimantName} <br> ${responseClaim.address1} <br> ${responseClaim.city.name} <br> 
-        ${responseClaim.postalCode.name} <br> ${responseClaim.phone} <br> ${responseClaim.email} <br>  <br> 
+        BOL: ${claim.bolNumber} <br> PRO: ${claim.proNumber} <br> Claimant Information <br> 
+        ${claim.claimantName} <br> ${claim.address1} <br> ${claim.city.name} <br> 
+        ${claim.postalCode.name} <br> ${claim.phone} <br> ${claim.email} <br>  <br> 
 
         We will update you as soon as possible.<br>
         Thank you for your understanding.<br><br>
         </p>`
-    }, (err) => {
-      if (err) {
-        res.badRequest({
-          message: "Error sending email."
-        });
-      } else {
-        res.ok(claim);
-      }
-    });
-    // } catch (error) {
-    //   res.badRequest({
-    //     error
-    //   });
-    // }
+      }, (err) => {
+        if (err) {
+          res.badRequest({
+            message: "Error sending email."
+          });
+        } else {
+          res.ok(claim);
+        }
+      });
+    } catch (error) {
+      res.badRequest({
+        error
+      });
+    }
   },
 
 };
