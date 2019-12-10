dd836cd7f52f7f16a12753a4545a845b38d47a9e
Zawar Shahid <zawar_shahid93@hotmail.com>
2019-12-10 18:10:05 +0500

    Socket events added for pickup notification

--- api/controllers/NotificationsController.js
+++ api/controllers/NotificationsController.js
@@ -53,11 +53,56 @@ io.on('connection', socket => {
           });
         });
       }
+    } else if (data.module == 'pickup') {
+      // let pickup = await Orders.findOne({
+      //   id: data.moduleItemId
+      // }).populateAll();
+
+      let users = await User.find({
+        roles: ['Admin']
+      });
+
+      if (users.length > 0) {
+        let notificationsList = [];
+
+        users.map(user => {
+          notificationsList.push({
+            ...data,
+            owner: user.id
+          });
+        })
+
+        let ids = [];
+        await Notifications.createEach(notificationsList);
+        notificationsList.map(val => ids.push(val._id.toString()));
+
+        notificationsList = await Notifications.find({
+          id: ids
+        }).populateAll();
+
+        notificationsList.forEach((notifObj, idx) => {
+          RedisService.get(`socket-${notifObj.owner.id}`, result => {
+            if (result != undefined) {
+              notificationsList[idx].socketId = result;
+            }
+
+            if (idx == notificationsList.length - 1) {
+              notificationsList.map(obj => {
+                io.to(obj.socketId).emit('notification', obj)
+              });
+            }
+          });
+        });
+      }
     }
   });
 
-  socket.on('fetchNotifications', data => {
+  socket.on('fetchNotifications', async data => {
+    let notifications = await Notifications.find({
+      owner: data.userId
+    }).limit(3).populateAll();
 
+    socket.emit('fetchNotifications', notifications);
   });
 });
 
